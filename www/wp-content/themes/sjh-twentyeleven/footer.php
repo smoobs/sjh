0f2a16d756c87e36f802a9925b6c0b0be080b32b
Sam Urquhart <sam@mrstth.com>
2015-10-02 16:36:00 +0100

    customising the twenty eleven theme to Leo's design; installing recent revisions plugin;

---
+++ www/wp-content/themes/sjh-twentyeleven/footer.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Template for displaying the footer
+ *
+ * Contains the closing of the id=main div and all content after
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+?>
+
+	</div><!-- #main -->
+
+	<footer id="colophon" role="contentinfo">
+
+			<?php
+				/*
+				 * A sidebar in the footer? Yep. You can can customize
+				 * your footer with three columns of widgets.
+				 */
+				if ( ! is_404() )
+					get_sidebar( 'footer' );
+			?>
+
+			<nav id="bottom-access" role="navigation">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
+			</nav><!-- #bottom-access -->
+			<div id="site-generator">
+				
+			</div>
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ www/wp-content/themes/sjh-twentyeleven/sidebar.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+$options = twentyeleven_get_theme_options();
+$current_layout = $options['theme_layout'];
+
+if ( 'content' != $current_layout ) :
+?>
+		<div id="secondary" class="widget-area" role="complementary">
+			<?php (dynamic_sidebar( 'sidebar-1' ) )  ?>
+
+		</div><!-- #secondary .widget-area -->
+<?php endif; ?>
